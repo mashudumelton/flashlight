1d7ee997e81a2bc5741500d6e5d3f56c253b33df
mashudumelton <mashudumelton@gmail.com>
2018-08-02 09:06:45 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Camera } from '@ionic-native/camera';
+import { Flashlight } from '@ionic-native/flashlight';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -25,7 +25,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    Camera
+    Flashlight
   ]
 })
 export class AppModule {}
